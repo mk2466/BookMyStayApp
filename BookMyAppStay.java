d52c344e3dec7b6705a386d563bde26f0a9be543
mk2466 <manishkumarsekwal@gmail.com>
2026-03-12 15:50:57 +0530

    Add files via upload

---
+++ BookMyAppStay.java
@@ -0,0 +1,10 @@
+public class BookMyAppStay{
+    public static void main(String[] args) {
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+        System.out.println("Welcome to " + appName);
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Application Version: " + version);
+    }
+}
